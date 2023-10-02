9eb3b3b4f4bf92c3997aa3ddeb1493c14add4277
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2023-10-02 09:15:08 +0530

    sm8475-common: Include hardware/google/interfaces and hardware/google/pixel
     * Required by power-libperfmgr
    
    Change-Id: I4053938614be26c7066ea83d97955739f39b4144

--- common.mk
+++ common.mk
@@ -319,6 +319,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/motorola \
     hardware/qcom-caf/common/libqti-perfd-client
